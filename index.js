b44f4a27996ef9163f1b1d9dfbbb52f5cf431183
odunet <odunet2000@yahoo.com>
2021-05-07 17:04:59 +0100

    Updated Express endpoint

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 // Sends email using the zuriMail module and Mail Trap
 const express = require('express');
+const path = require('path');
 const { sendMail, options } = require('./zuriMailTraining');
 
 let app = express();
+var router = express.Router();
 let PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({
-      data:
-        'I am the best, the post endpoint is used to send email using zuriMailTraining module',
-    });
+  res.status(200).json({
+    data:
+      'I am the best, the post endpoint is used to send email using zuriMailTraining module',
+  });
+});
+app.get('/form', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.post('/', async (req, res) => {
+app.post('/api', async (req, res) => {
   options.subject = req.body.subject;
   options.email = req.body.email;
   try {
@@ -26,6 +31,12 @@ app.post('/', async (req, res) => {
   } catch (e) {
     res.status(504).json({ Status: 'Not Sent', Error: e });
   }
+});
+
+app.use('/api', router);
+app.use(function (req, res, next) {
+  console.log('Time of request:', Date.now());
+  next();
 });
 
 app.listen(PORT, () => {
